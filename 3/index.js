e923abc1fc24846d733e38131c2bb737205a7c22
Brad Murray <bmurray@fitbit.com>
2017-12-06 00:19:56 -0500

    Part two of day 3!

--- 3/index.js
+++ 3/index.js
@@ -70,9 +70,135 @@ function getSpiralDistance(value) {
   return ringNumber + ringPositionDistance;
 }
 
-var getSpiralDistanceTests = { 1: 0, 12: 3, 23: 2, 1024: 31, 325489: "?" };
-for (var key in getSpiralDistanceTests) {
-  console.log("getSpiralDistanceTests: " + key + " s/b " +
-              getSpiralDistanceTests[key] + ", is " +
-              getSpiralDistance(key));;
+//var getSpiralDistanceTests = { 1: 0, 12: 3, 23: 2, 1024: 31, 325489: "?" };
+//for (var key in getSpiralDistanceTests) {
+//  console.log("getSpiralDistanceTests: " + key + " s/b " +
+//              getSpiralDistanceTests[key] + ", is " +
+//              getSpiralDistance(key));;
+//}
+
+// Part 2
+
+function getIndexForIndexOnRing(indexOnRing, ring) {
+  // indexOnRing is defined as the 0th position being the lowest value on 
+  // the ring (one spot above the highest value at the bottom right corner)
+
+  if (ring == 0) {
+    return 0;
+  }
+
+  var ringStartIndex = Math.pow(((ring - 1) * 2) + 1, 2);
+  return ringStartIndex + indexOnRing;
 }
+
+//var getIndexForIndexOnRingTests = [[0, 0, 0], [0, 1, 1], [1, 1, 1], [0, 2, 9], [1, 2, 10]]
+//for (var key in getIndexForIndexOnRingTests) {
+//  var test = getIndexForIndexOnRingTests[key]
+//  console.log("getIndexForIndexOnRingTests: " + test + ", is " +
+//              getIndexForIndexOnRing(test[0], test[1]));
+//}
+
+function getIndexOnRing(index) {
+  if (index == 0) {
+    return 0;
+  }
+
+  var ring = getRingNumber(index + 1);
+  var ringStartIndex = Math.pow(((ring - 1) * 2) + 1, 2);
+  return index - ringStartIndex;
+}
+
+//var getIndexOnRingTests = [[0, 0], [1, 0], [2, 1], [9, 0], [10, 1]];
+//for (var key in getIndexOnRingTests) {
+//  var test = getIndexOnRingTests[key]
+//  console.log("getIndexOnRingTests: " + test + ", is " +
+//              getIndexOnRing(test[0]));
+//}
+
+function calculateValueForIndex(values, index) {
+  if (index == 0) {
+    return 1;
+  }
+
+  var ring = getRingNumber(index + 1);
+  var indexOnRing = getIndexOnRing(index);
+  var sideLength = (ring * 2); // Only count one corner
+
+  if (indexOnRing == 0) {
+    // First value on the ring, only thing to include is the first and last
+    // values on the previous ring.
+
+    // Last value
+    var result = getValueForIndex(values, index - 1);
+    // First value (only include for rings where the first and last aren't the same)
+    if (ring > 1) {
+      result += getValueForIndex(values, getIndexForIndexOnRing(0, ring - 1));
+    }
+
+    return result;
+  }
+
+  var result = getValueForIndex(values, index - 1);
+
+  // If we're coming off a corner, we include two previous values on the ring
+  // Note on on indexOnRing == 1 we'll underflow into the previous ring, and
+  // that's intended.
+  if (index > 1 && (indexOnRing % sideLength == 0 || indexOnRing == 1)) {
+    result += getValueForIndex(values, index - 2);
+  }
+ 
+  // And now, add the values that are diagonally infront, to the side, and
+  // diaganolly behind on the previous ring
+
+  var side = Math.floor(indexOnRing / sideLength);
+  var indexOnSide = indexOnRing % sideLength;
+
+  var previousRingStartIndex = Math.pow(((ring - 2) * 2) + 1, 2);
+  var previousRingSideLength = (ring - 1) * 2;
+  var previousRingStartIndexOfSide = previousRingStartIndex + (previousRingSideLength * side);
+
+  if (indexOnSide > 0 && indexOnRing > 1) {
+    result += getValueForIndex(values, previousRingStartIndexOfSide + indexOnSide - 2)
+  }
+  if (indexOnSide < sideLength - 1) {
+    result += getValueForIndex(values, previousRingStartIndexOfSide + indexOnSide - 1)
+  }
+  if (indexOnSide < sideLength - 2) {
+    result += getValueForIndex(values, previousRingStartIndexOfSide + indexOnSide)
+  }
+
+  // If we're wrapping up the ring, we start including the first number in the
+  // ring again.
+  var startIndexOnRing = Math.pow(((ring - 1) * 2) + 1, 2);
+  var lastIndexOnRing = Math.pow((ring * 2) + 1, 2) - 1;
+  if (index > lastIndexOnRing - 2) {
+    result += getValueForIndex(values, startIndexOnRing);
+  }
+
+  return result;
+}
+
+function getValueForIndex(values, index) {
+  if (values[index] !== undefined) {
+    // Already calculated it
+    return values[index];
+  }
+
+  // Welp, gotta calculate it now
+  var result = calculateValueForIndex(values, index);
+
+  // Save it, return it
+  values[index] = result;
+  return result;
+}
+
+var values = [];
+for (var i = 0; ; i++) {
+  console.log(i);
+  var result = getValueForIndex(values, i);
+  if (result > 325489) {
+    console.log(result);
+    break;
+  }
+}
+//console.log(values);
